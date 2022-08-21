70564f44e3a4c033b659ab5d897f276fcd2c3284
Maximilian Schiedermeier <maximilian.schiedermeier@mail.mcgill.ca>
2022-08-21 16:55:35 -0400

    increased test coverage

--- src/test/java/SvGPatcherTest.java
+++ src/test/java/SvGPatcherTest.java
@@ -1,3 +1,4 @@
+import eu.kartoffelquadrat.svgpatcher.CssPatcher;
 import eu.kartoffelquadrat.svgpatcher.IdPatcher;
 import eu.kartoffelquadrat.svgpatcher.XmlIOUtils;
 import org.junit.Assert;
@@ -24,6 +25,8 @@ public class SvGPatcherTest {
     public static final File TEST_INPUT_GRAPHIC = new File(TEST_RESOURCE_FOLDER.getAbsolutePath() + "/testgraphic.svg");
     public static final File TEST_IDPATCHED_OUTPUT_LOCATION = new File(TEST_RESOURCE_FOLDER.getAbsolutePath() + "/idpatched.svg");
     public static final File TEST_IDPATCHED_REFERENCE_LOCATION = new File(TEST_RESOURCE_FOLDER.getAbsolutePath() + "/idpatchedreference.svg");
+    public static final File TEST_CSSPATCHED_OUTPUT_LOCATION = new File(TEST_RESOURCE_FOLDER.getAbsolutePath() + "/csspatched.svg");
+    public static final File TEST_CSSPATCHED_REFERENCE_LOCATION = new File(TEST_RESOURCE_FOLDER.getAbsolutePath() + "/csspatchedreference.svg");
 
 
     /**
@@ -38,20 +41,55 @@ public class SvGPatcherTest {
      * Test if the string (not persisted to disk) when sample svg is only ID patched complies to what we expect.
      */
     @Test
-    public void testIdPatch() throws IOException, SAXException, ParserConfigurationException, TransformerException, NoSuchAlgorithmException {
+    public void testIdPatch() throws IOException, SAXException, ParserConfigurationException, TransformerException {
         Document svg = XmlIOUtils.parseXmlToDocument(TEST_INPUT_GRAPHIC.getAbsolutePath());
 
-        // Patch all IDs
+        // Patch all IDs. This is the transformation we want to test
         IdPatcher.patchAllOmnigraffleIds(svg);
 
+        // Verify the persisted svg content is as expected (verifies the above patch worked as expected)
+        exportAndCompareToExpected(svg, TEST_IDPATCHED_OUTPUT_LOCATION, TEST_IDPATCHED_REFERENCE_LOCATION);
+    }
+
+    /**
+     * Test if the string (not persisted to disk) when sample svg is only ID patched complies to what we expect.
+     */
+    @Test
+    public void testCssPatch() throws IOException, SAXException, ParserConfigurationException, TransformerException, NoSuchAlgorithmException {
+        Document svg = XmlIOUtils.parseXmlToDocument(TEST_INPUT_GRAPHIC.getAbsolutePath());
+
+        // Patch all IDs. This is the transformation we want to test
+        CssPatcher.patchSvgCss(svg);
+
+        // Verify the persisted svg content is as expected (verifies the above patch worked as expected)
+        exportAndCompareToExpected(svg, TEST_CSSPATCHED_OUTPUT_LOCATION, TEST_CSSPATCHED_REFERENCE_LOCATION);
+    }
+
+    /**
+     * Helper method to export a patched svg to disk and test its content corresponds to the expected reference file.
+     *
+     * @param svg       as a modified svg
+     * @param output    as the location where to store the patched file
+     * @param reference as the location of the reference file
+     */
+    private void exportAndCompareToExpected(Document svg, File output, File reference) throws IOException, TransformerException {
+
+        // Remove any trace of exported file from a previous test run
+        File legacyExportFile = new File(output.getAbsolutePath());
+        if (legacyExportFile.exists())
+            legacyExportFile.delete();
+
         // Write the outcome to disk
-        XmlIOUtils.writeXmlDocumentToDisk(svg, TEST_IDPATCHED_OUTPUT_LOCATION.getAbsolutePath());
+        XmlIOUtils.writeXmlDocumentToDisk(svg, output.getAbsolutePath());
 
         // Verify this corresponds to the expected outcome
-        String generatedContent = new Scanner(TEST_IDPATCHED_OUTPUT_LOCATION).useDelimiter("\\Z").next();
-        String expectedContent = new Scanner(TEST_IDPATCHED_REFERENCE_LOCATION).useDelimiter("\\Z").next();
+        String generatedContent = new Scanner(output).useDelimiter("\\Z").next();
+        String expectedContent = new Scanner(reference).useDelimiter("\\Z").next();
 
-        Assert.assertTrue("Generated file with patched ids differs from expected output. Run this command to inspect how they differ: \nicdiff " + TEST_IDPATCHED_OUTPUT_LOCATION.getAbsolutePath() + " " + TEST_IDPATCHED_REFERENCE_LOCATION.getAbsolutePath(), generatedContent.equals(expectedContent));
-
+        Assert.assertTrue("Generated file with patched ids differs from expected output. Run this command to" +
+                        " inspect how they differ: \nicdiff " + output.getAbsolutePath() + " " + reference.getAbsolutePath(),
+                generatedContent.equals(expectedContent));
     }
+
+
 }
